0866f4bff4ec412b88589938758a320b897dc921
Marcos Amorim <126255748+mkdev-2@users.noreply.github.com>
2024-12-13 17:19:26 -0300

    up to production v5

--- app/previa/page.tsx
+++ app/previa/page.tsx
@@ -50,10 +50,10 @@ export default function VisualizationPage() {
     const produtosStr = localStorage.getItem("produtos");
     const storedColorScheme = localStorage.getItem("selectedColor");
     if (background) {
-        setMainBackground(`https://encarte-be.likedsg.com/${background}`);
+        setMainBackground(`https://encarte-be.likedsg.com${background}`);
       }
       if (secBackground) {
-        setSubsequentBackground(`https://encarte-be.likedsg.com/${secBackground}`);
+        setSubsequentBackground(`https://encarte-be.likedsg.com${secBackground}`);
       }
     if (produtosStr) {
       const produtos: Produto[] = JSON.parse(produtosStr);
